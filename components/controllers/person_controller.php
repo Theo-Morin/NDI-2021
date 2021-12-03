56626f554603d1b8f8512ca6757c015d714ea4b6
Berthier Théo <berthiertheo@hotmail.fr>
2021-12-03 04:42:30 +0100

    ajout controlleur person boat

--- components/controllers/person_controller.php
+++ components/controllers/person_controller.php
@@ -4,18 +4,82 @@ $uc2 = isset($_GET['uc2']) ? htmlspecialchars($_GET['uc2']) : exit(header('Locat
 switch($uc2) {
     case "create":
         $include = "person/form";
-        $title = "Créér un nouveau Bateau";
+        $title = "Créér une nouvelle personne";
+        $user = new User();
+        if($user->isLogged()){
+            if(isset($_POST['fullname']) && isset($_POST['grade']) && isset($_POST['content']) && isset($_POST['pics']) && isset($_POST['birth']) && isset($_POST['death']) && isset($_POST['gender'])){
+                $fullname = htmlspecialchars($_POST['fullname']);
+                $grade = htmlspecialchars($_POST['grade']);
+                $content = htmlspecialchars($_POST['content']);
+                $pics = ($_FILES['pics']);
+                $birth = htmlspecialchars($_POST['birth']);
+                $death = htmlspecialchars($_POST['death']);
+                $gender = htmlspecialchars($_POST['gender']);
+                Person::create($fullname,$grade,$content,$pics,$birth,$death,$gender);
+            }
+            else{
+                Notification::create("warning","tout les champs sont nécessaires");
+            }
+    }
+    else{
+        Notification::create("warning","veuillez vous connecter");
+        exit(header('Location: /contributor/login'));
+    }
     break;
     case "update":
         $uc3 = isset($_GET['uc3']) ? htmlspecialchars($_GET['uc3']) : exit(header('Location: /home'));
         $include = "person/form";
-        $title = "";
+        $title = "mêttre à jour une personne";
+        $user = new User();
+        if($user->isLogged()){
+            if(isset($_POST['fullname']) && isset($_POST['grade']) && isset($_POST['content']) && isset($_POST['birth']) && isset($_POST['death']) && isset($_POST['gender']) && isset($_POST['id'])){
+                $fullname = htmlspecialchars($_POST['fullname']);
+                $grade = htmlspecialchars($_POST['grade']);
+                $content = htmlspecialchars($_POST['content']);
+                $pics = ($_FILES['pics']);
+                $birth = htmlspecialchars($_POST['birth']);
+                $death = htmlspecialchars($_POST['death']);
+                $gender = htmlspecialchars($_POST['gender']);
+                $id = htmlspecialchars($_POST['id']);
+                $personne = new Person($id);
+                $personne->update($fullname,$grade,$content,$pics,$birth,$death,$gender);
+            }
+            else{
+                Notification::create("warning","tout les champs sont nécessaires");
+            }
+        }
+        else{
+            Notification::create("warning","veuillez vous connecter");
+            exit(header('Location: /contributor/login'));
+        }
     break;
     case "validate":
         $uc3 = isset($_GET['uc3']) ? htmlspecialchars($_GET['uc3']) : exit(header('Location: /home'));
+        if(isset($_POST['id']) && isset($_POST['validate'])){
+            $id = htmlspecialchars($_POST['id']);
+            $personne = new Person($id);
+            if($personne->validate()){
+                exit(header('Location: /admin/person'));
+            }
+
+        }
+        else{
+            Notification::create("warning","tout les champs sont nécessaires");
+        }
+
     break;
     case "delete":
         $uc3 = isset($_GET['uc3']) ? htmlspecialchars($_GET['uc3']) : exit(header('Location: /home'));
+        if(isset($_POST['id']) && isset($_POST['delete'])){
+            $id = htmlspecialchars($_POST['id']);
+            $personne = new Person($id);
+            if($personne->delete()){
+                exit(header('Location: /admin/person'));
+            }
+        }
+        else{
+            Notification::create("warning","tout les champs sont nécessaires");
+        }
     break;
     default:
         $uc3 = isset($_GET['uc3']) ? htmlspecialchars($_GET['uc3']) : exit(header('Location: /home'));
